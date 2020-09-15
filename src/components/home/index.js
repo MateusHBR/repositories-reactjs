423138f0e2844b8386605cbb66ef5d62e5e54bf6
MateusHBR <mateusmorais.r@gmail.com>
2020-09-15 18:16:31 -0300

    cria component home

---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import RepositoryComponent from './Repository-component';
+import api from '../../services/api';
+import './styles.css';
+
+class Home extends Component {
+
+
+}
+
+export default Home;
